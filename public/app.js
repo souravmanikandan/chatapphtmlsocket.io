11c38ff67ac3abde9c9ca5fbed7985b3b85d7eac
smcoderr <souravoz2018@gmail.com>
2024-12-07 17:48:57 +0530

    2

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500')
+const socket = io('ws://https://chatapphtmlsocket-io.onrender.com/')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
