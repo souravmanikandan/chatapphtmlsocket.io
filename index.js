aa7b3f26a1b1beade5ebdd06e18c12ca64bf8e8d
smcoderr <souravoz2018@gmail.com>
2024-12-07 17:58:38 +0530

    3

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const UsersState = {
 
 const io = new Server(expressServer, {
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500","https://chatapphtmlsocket-io.onrender.com"]
     }
 })
 io.on('connection', socket => {
